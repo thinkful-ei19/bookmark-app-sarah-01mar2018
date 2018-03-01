710d93e7a73145932751d09846daa50689148c14
ssheaks <sarah.sheaks@gmail.com>
2018-03-01 09:57:00 -0800

    finish? base html

---
+++ scripts/bookmarksList.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// const bookmarksList = (function(){
+
+//   //Below: inject HTML, create bookmark string to render, render to the DOM
+
+//   function generateBookmarkElement(bookmark) {
+//   //HTML goes here
+
+//     return `<div class='bookmark-item condensed' data-id=${bookmark.id}>
+//     <p><h3 class='bookmark-title'>${bookmark.title}</h3>
+//     <span class='bookmark-rating'>${bookmark.rating}</span></p>
+//     <div class="bookmark-controls">
+//           <button class="bookmark-expand js-bookmark-expand">
+//             <span class="button-label">expand bookmark</span>
+//           </button>
+//           <button class="bookmark-delete js-bookmark-delete">
+//             <span class="button-label">delete</span>
+//           </button>
+//         </div>
+//     </div>`;
+//   }
+// }());
