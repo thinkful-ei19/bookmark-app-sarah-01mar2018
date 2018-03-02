78196417f41beadc2162662922583a6495514952
ssheaks <sarah.sheaks@gmail.com>
2018-03-02 11:23:20 -0800

    moved header html to bookmarksList and broke event listener

--- scripts/bookmarksList.js
+++ scripts/bookmarksList.js
@@ -72,10 +72,60 @@ const bookmarksList = (function(){
     console.log(bookmarks);
   }
 
+  //generate default header HTML
+  const generateDefaultHeader = function(){
+    return `
+    <div class='toggle-add add-bookmark-button js-add-bookmark-button'>
+     <!-- <form id="js-add-button-form"> -->
+          <button class='js-add-button'>Add bookmark</button>
+        <!-- </form> -->
+      </div>
+      <div class='min-rating-menu'>
+        <label class='minimum rating' for="filter-rating">Minimun Rating</label>
+        <select name = "minimum rating" id='minimum-rating'>
+          <option value='1' selected>1</option>
+          <option value='2'>2</option>
+          <option value='3'>3</option>
+          <option value='4'>4</option>
+          <option value='5'>5</option>
+        </select>
+      </div>
+    `
+  }
+
+  const generateAddBookmarkHeader = function() {
+    return `
+    <form action='#' class='toggle-add add-new-bookmark-form js-new-bookmark-form hidden'>
+        <h2>Add a new bookmark</h2>
+        <button type="submit" id='new-bookmark-submit'> Submit</button>        
+        <label>Title</label>
+        <input type="text" name="title" id="title" placeholder="add title">
+        <label>Web Address</label>
+        <input type="text" name="url" id="url" placeholder="url">
+        <label>Description</label>
+        <input type="text" name="desc" id="desc" placeholder="Enter a description">
+        <div id='rating'>
+            <label>Rate site</label>
+            <input type='radio' name='rating' data-radio-rating=1 value='1'><label for='rating'>1</label>
+            <input type='radio' name='rating' data-radio-rating=2 value='2'><label for='rating'>2</label>
+            <input type='radio' name='rating' data-radio-rating=3 value='3'><label for='rating'>3</label>
+            <input type='radio' name='rating' data-radio-rating=4 value='4'><label for='rating'>4</label>
+            <input type='radio' name='rating' data-radio-rating=5 value='5'><label for='rating'>5</label>
+        </div>
+
+       
+        <!-- <input type="button" name='cancel' value='cancel' onClick='https://thinkful-list-api.herokuapp.com/sarah/bookmarks' /> -->
+      </form>
+    `
+  }
+
   // //This is what will be rendered to the DOM
   function render() {
     let bookmarks = store.bookmarks;
 
+    let header = (store.adding) ? generateAddBookmarkHeader() : generateDefaultHeader();
+    //let currentStore = (store.ratingFilter === 'all') ? store.bookmarks : store.bookmarks.filter(obj => obj.rating >= store.ratingFilter);
+    
     // if (store.filterRating !== null) {
     //   bookmarks = store.bookmarks.filter( => {
     //     return bookmarkValues.rating >= store.filterRating;
@@ -88,18 +138,19 @@ const bookmarksList = (function(){
     const bookmarksString = generateBookmarkListString(bookmarks);
    
     // inserts HTML into the DOM
+    $('.js-header-add-filter').html(header);
     $('.js-bookmarks-list').html(bookmarksString);
-  }
+}
   
-  //form values to use in other functions
-  const bookmarkVals = function() {
-    return {
-      title: $('#title').val(),
-      url: $('#url').val(),
-      desc: $('#desc').val(),
-      rating: $(parseInt($('#rating')))
-    };
-  };
+  // //form values to use in other functions
+  // const bookmarkVals = function() {
+  //   return {
+  //     title: $('#title').val(),
+  //     url: $('#url').val(),
+  //     desc: $('#desc').val(),
+  //     rating: $(parseInt($('#rating')))
+  //   };
+  // };
 
   //gets the bookmark id for the bookmark clicked on
   function getIdFromElement(bookmark) {
@@ -180,10 +231,6 @@ const bookmarksList = (function(){
   /*function handleAddBookmarkCancel() {
 
   }
-
-function handleBookmarkExpand() {
-
-}
 
 
 function handleBookmarkMinRating() {
